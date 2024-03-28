0b3f018f3b6b1a194aacea2a5b824c3d98673e15
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-03-29 01:26:49 +0200

    add personal info component

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header.jsx';
+import PersonalInfo from './components/PersonalInfo.jsx';
 
 export default function App() {
   return (
     <article>
       <Header />
+      <PersonalInfo />
     </article>
   )
 }
