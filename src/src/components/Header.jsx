2b58ae4ba74eecdc2032504ffa710ac63a695ef8
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-03-30 01:58:27 +0200

    add basic styles for the buttons

--- src/src/components/Header.jsx
+++ src/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import facePhoto from '../assets/face.png';
+import btnIcons from '../assets/btn-icons.svg';
 
 export default function Header() {
   return (
@@ -10,8 +11,18 @@ export default function Header() {
       </h2>
       <p>laurasmith.website</p>
       <div>
-        <button className="card-btn card-email-btn">Email</button>
-        <button className="card-btn card-linkedin-btn">LinkedIn</button>
+        <button className="card-btn card-email-btn" aria-label="Email">
+          <svg width="16px" height="16px">
+            <use href={`${btnIcons}#mail`}></use>
+          </svg>
+          <span>Email</span>
+        </button>
+        <a href="https://linkedin.com" className="card-btn card-linkedin-btn" aria-label="LinkedIn">
+          <svg width="16px" height="16px">
+            <use href={`${btnIcons}#linkedin`}></use>
+          </svg>
+          <span>LinkedIn</span>
+        </a>
       </div>
     </header>
   )
