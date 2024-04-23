9851a0ce51569b5d805b3a3458920aa66ab896f1
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-04-23 21:01:35 +0300

    add link to github repo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,15 @@ import Footer from './Footer.jsx';
 
 export default function App() {
   return (
-    <article className="card">
-      <Header />
-      <PersonalInfo />
-      <Footer />
-    </article>
+    <>
+      <article className="card">
+        <Header />
+        <PersonalInfo />
+        <Footer />
+      </article>
+      <footer>
+        <p className="github-info"><a href="https://github.com/tasxatzial/digital-business-card" className="github-link">See the project on GitHub</a></p>
+      </footer>
+    </>
   )
 }
