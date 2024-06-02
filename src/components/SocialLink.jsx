7b0bd3890597b9c65748447b2f6d8ceef7b39292
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-06-02 22:25:51 +0300

    add generic focus,hover styles

--- src/components/SocialLink.jsx
+++ src/components/SocialLink.jsx
@@ -3,7 +3,7 @@ import SocialIcons from '../assets/social-icons.svg';
 
 export default function SocialLink(props) {
   return (
-    <a href={`https://${props.name}.com`} aria-label={props.name}>
+    <a href={`https://${props.name}.com`} className="card-social-link" aria-label={props.name}>
       <svg className="card-social-link-icon" width="25px" height="25px">
         <use href={`${SocialIcons}#${props.name}`}></use>
       </svg>
