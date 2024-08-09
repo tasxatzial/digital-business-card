e71557aa2c62df5058dc1f444abd757f1a07672d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-08-10 00:38:59 +0300

    add github workflow

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: "src",
+  base: "/digital-business-card/",
   build: {
     emptyOutDir: true,
     outDir: '../dist',
